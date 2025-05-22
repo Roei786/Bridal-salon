25287e435d8b13048ff1fc8d1cec45111bb5564f
Roei786 <Roei7342@gmail.com>
2025-05-22 15:11:20 +0300

    Added employee dashboard

---
+++ src/components/EmployeeDashboard.jsx
@@ -0,0 +1,9 @@
+export default function EmployeeDashboard() {
+  return (
+    <div className="dashboard-card">
+      <h2 className="dashboard-title">ברוך הבא עובד</h2>
+      <p>כאן תוכל לעדכן טפסים, לראות משימות ועוד.</p>
+      {/* תוסיף כאן כל קומפוננטה שתרצה לעובד */}
+    </div>
+  );
+}
